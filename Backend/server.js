1c9df7246f15003ebe35a090efdf1f29f91e2310
Aryanshokeen14 <aryanshokeen14@gmail.com>
2025-08-27 14:23:10 +0530

    app.get(/). added in backend

--- Backend/server.js
+++ Backend/server.js
@@ -10,6 +10,11 @@ const PORT = 3000;
 app.use(express.json());
 app.use(cors());
 
+app.get("/", (req, res) => {
+  res.send("🚀 API is running... use /api endpoints");
+});
+
+
 app.use("/api", chatRoutes);
 
 
